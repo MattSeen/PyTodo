391ccbfc9fd1d9ce533c858ae138edd8278786b9
Matt Cunningham <matt.c1986@gmail.com>
2012-04-17 23:59:02 +0100

    fix input bugs

--- main.py
+++ main.py
@@ -54,29 +54,38 @@ def deleteEntry():
         count += 1
     
     print ""
-    taskToRemove = int(raw_input("Choose a task to remove: "))
-    
-    if( taskToRemove in indexList ):
-        del(taskList[indexList[int(taskToRemove)]])
+    try:    
+        taskToRemove = int(raw_input("Choose a task to remove: "))
         
+        if( taskToRemove in indexList ):
+            del(taskList[indexList[int(taskToRemove)]])
+    except ValueError:
+        print 'I cannot delete that task'
+        raw_input("Press enter to continue")
+    
 def mainLoop():
     while(1):
         printGUI()
         print "1) Refresh"
         print "2) New Entry"
         print "3) Delete Entry"
-        print "q) Exit"
+        print "0) Exit"
         print ""
 
         action = raw_input("Choose an option: ")        
         
-        if( isinstance(action,str)):
-            if(action == "q"):
-                break #Kill program
-        else:
+        try:
             if (int(action) in performAction):
                 performAction[int(action)]()
-    
+            elif(int(action) == 0):
+                break
+            else:
+                print "@....<--- tumble weed"
+        except ValueError:
+            print 'Please supply integer arguments'
+            raw_input("Press enter to continue")
+        
+
 performAction = {
     1: refresh,
     2: addEntry,
